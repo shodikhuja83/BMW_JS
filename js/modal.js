434d67befd979719f38c7b9d88316db03d2ab0a6
shodikhuja83 <shodikhuja83@gmail.com>
2021-05-20 10:40:04 +0500

    day 3

---
+++ js/modal.js
@@ -0,0 +1,21 @@
+const moreElems = document.querySelectorAll('.more');
+const modalElem = document.querySelector('.modal');
+
+moreElems.forEach((moreElem) => {
+	const openModal = () => {
+		modalElem.classList.remove('hidden');
+	};
+
+	const closeModal = () => {
+		modalElem.classList.add('hidden');
+	};
+
+	moreElem.addEventListener('click', openModal);
+	modalElem.addEventListener('click', (event) => {
+		const target = event.target;
+
+		if (target.classList.contains('overlay') || target.classList.contains('modal__close')) {
+			closeModal()
+		}
+	})
+})
